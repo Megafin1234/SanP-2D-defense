85aa93d0d5749140c17ace73fc7eebbd2e60201d
Seunghun <eric0h@naver.com>
2024-10-09 17:05:29 +0900

    Initial commit

---
+++ Assets/Undead Survivor/Scripts/PoolManager.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PoolManager : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    List<GameObject>[] pools;
+
+    void Awake()
+    {
+        pools = new List<GameObject>[prefabs.Length]; //배열초기화
+
+        for (int index=0;index<pools.Length;index++){
+            pools[index] = new List<GameObject>(); //배열안의 리스트 초기화
+        }
+
+        Debug.Log(pools.Length);
+    }
+
+    public GameObject Get(int index){
+        GameObject select = null;
+        foreach(GameObject item in pools[index]){
+            if (!item.activeSelf){
+                select = item;
+                select.SetActive(true);
+                break;
+            }
+        }
+        if (!select){
+            select = Instantiate(prefabs[index],transform);
+            pools[index].Add(select);
+        }
+        return select;
+    }
+}
