3ce243153025015a09d60708abaa732465da6b82
KGB <cjsekomp@naver.com>
2025-04-09 16:45:53 +0900

    add crosshair & dash to crosshair(not keyboard moving)

--- Assets/Undead Survivor/Scripts/GameManager.cs
+++ Assets/Undead Survivor/Scripts/GameManager.cs
@@ -52,10 +52,14 @@ public class GameManager : MonoBehaviour
 
     public List<GameObject> party;
 
+    public Texture2D crosshair;
+    public Vector2 hotspot = new Vector2(16, 16);
+
     private bool isDayPhase = true;
 
     void Awake(){
         instance = this;
+        Cursor.SetCursor(crosshair, hotspot, CursorMode.Auto); 
     }
     public void GameStart(int id)
     {
